da4b09a1c55eef2dbb757ebf7c8b01ee3795b592
Lauren <lauren@wnkinc.co>
2019-12-12 14:43:34 -0800

    Initial commit of ObjectPool and Wave classes

---
+++ Scripts/Wave.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create New Wave", fileName = "New Wave")]
+public class Wave : ScriptableObject
+{
+  public int NumberOfEnemies;
+  public List<Enemy> WaveEnemies = new List<Enemy>();
+
+  public WaveFormation Formation;
+}
