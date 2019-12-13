d6a1a52a3a13470379dbdd024fe5e8d11899b4d9
Lauren <lauren@wnkinc.co>
2019-12-12 16:18:17 -0800

    Imported from generic object pool repo; implemented pool in enemypool

--- Scripts/ObjectPool.cs
+++ Scripts/ObjectPool.cs
@@ -1,4 +1,41 @@
-public class ObjectPool
+using UnityEngine;
+using System.Collections.Generic;
+
+public abstract class ObjectPool<T> : MonoBehaviour where T : Component
 {
-  
+  [SerializeField] private protected T _prefab;
+  [SerializeField] private protected int _poolCount;
+  [SerializeField] private protected bool _isPoolParent;
+  private Queue<T> _objectPool = new Queue<T>();
+  public Transform Spawnpoint;
+
+#region Object Pooling Methods
+  protected void FillPool(int count)
+  {
+    for (int i = 0; i < count; i++)
+    {
+      var obj = Instantiate(_prefab);
+      obj.gameObject.SetActive(false);
+      ReturnToPool(obj);
+    }
+  }
+
+  public T NextPoolObject()
+  {
+    return _objectPool.Dequeue();
+  }
+
+  public void ReturnToPool(T obj)
+  {
+    _objectPool.Enqueue(obj);
+  }
+#endregion
+
+  protected void SetPoolParent(GameObject parent)
+  {
+    foreach (T obj in _objectPool)
+    {
+      obj.transform.SetParent(parent.transform);
+    }
+  }
 }
