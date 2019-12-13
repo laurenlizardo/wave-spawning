a5328efc1eb35afa520c3657d3b04bca8a9f1a6c
Lauren <lauren@wnkinc.co>
2019-12-13 12:24:39 -0800

    Moved variables around

--- Scripts/Wave.cs
+++ Scripts/Wave.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public abstract class Wave<T> : ScriptableObject where T : Component
 {
-  public int NumberOfWaveElements => WaveFormation.GetSpawnpoints().Count;
-  public List<T> WaveElements = new List<T>();
   public WaveFormation WaveFormation;
   public List<Transform> Spawnpoints => WaveFormation.GetSpawnpoints();
+  public int NumberOfWaveElements => WaveFormation.GetSpawnpoints().Count;
+  public List<T> WaveElements = new List<T>();
 }
