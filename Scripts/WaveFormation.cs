713b141c7be84a687bc935de217b382577246e0c
Lauren <lauren@wnkinc.co>
2019-12-13 12:07:24 -0800

    Removed WF states and queue; modified GetSpawnpoints to return a list

--- Scripts/WaveFormation.cs
+++ Scripts/WaveFormation.cs
@@ -1,31 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveFormation : MonoBehaviour
 {
-  public enum WaveState
+  public List<Transform> GetSpawnpoints()
   {
-    Loading,
-    Active,
-    Dead
-  }
+    List<Transform> list = new List<Transform>();
 
-  // Decide on list or queue approach
-  [SerializeField] public List<Transform> SpawnpointList = new List<Transform>();
-  [SerializeField] public Queue<Transform> SpawnpointQueue = new Queue<Transform>();
-
-  public void GetSpawnpoints()
-  {
     for (int i = 0; i < transform.childCount; i++)
     {
-      SpawnpointList.Add(transform.GetChild(i).transform);
-      SpawnpointQueue.Enqueue(transform.GetChild(i).transform);
+      list.Add(transform.GetChild(i).transform);
     }
-  }
 
-  private void Start()
-  {
-    GetSpawnpoints();
+    return list;
   }
 }
