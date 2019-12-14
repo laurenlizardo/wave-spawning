abfabb8fa7d7a58f958f8b9eb3f75de112a6503c
Lauren <lauren@wnkinc.co>
2019-12-13 20:02:22 -0800

    Fixed 'spawning' errors

--- Scripts/WaveSystem/EnemyWaveController.cs
+++ Scripts/WaveSystem/EnemyWaveController.cs
@@ -14,14 +14,14 @@ public class EnemyWaveController : WaveController<EnemyWave>
 
   public override IEnumerator SpawnWave()
   {
-    for (int i = 0; i < Waves.Count; i++)
+    for (int i = 0; i < Waves[_currentWaveIndex].WaveElements.Count; i++)
     {
-      GameObject obj = Waves[i].WaveElements[i].NextPoolObject();
-      obj.transform.position = Waves[i].Spawnpoints[i].transform.position;
+      GameObject obj = Waves[_currentWaveIndex].WaveElements[i].NextPoolObject();
+      obj.transform.position = Waves[_currentWaveIndex].Spawnpoints[i].transform.position;
       obj.transform.rotation = Quaternion.identity;
       obj.SetActive(true);
 
-      yield return new WaitForSeconds(Waves[i].SpawnDelay);
+      yield return new WaitForSeconds(Waves[_currentWaveIndex].SpawnDelay);
     }
   }
 }
