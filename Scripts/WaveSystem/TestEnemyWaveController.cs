1d904c02dee07d7d117f1becd510dc58159e2110
Lauren <lauren@wnkinc.co>
2019-12-14 13:17:24 -0800

    Updated test enemy wave assets; updated test enemy wave controller with mutable wave index

--- Scripts/WaveSystem/TestEnemyWaveController.cs
+++ Scripts/WaveSystem/TestEnemyWaveController.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class TestEnemyWaveController : WaveController<TestEnemyWave>
 {
+  public int WaveIndex;
+
+  private int _waveIndex
+  {
+    get 
+    {
+      if (WaveIndex > Waves.Count - 1) return 0;
+      return WaveIndex;
+    }
+  }
+
   private void Update()
   {
     if (Input.GetKeyDown(KeyCode.Space))
@@ -14,14 +25,14 @@ public class TestEnemyWaveController : WaveController<TestEnemyWave>
 
   public override IEnumerator SpawnWave()
   {
-    for (int i = 0; i < Waves[_currentWaveIndex].WaveElements.Count; i++)
+    for (int i = 0; i < Waves[_waveIndex].WaveElements.Count; i++)
     {
-      GameObject obj = Waves[_currentWaveIndex].WaveElements[i].NextPoolObject();
-      obj.transform.position = Waves[_currentWaveIndex].Spawnpoints[i].transform.position;
+      GameObject obj = Waves[_waveIndex].WaveElements[i].NextPoolObject();
+      obj.transform.position = Waves[_waveIndex].Spawnpoints[i].transform.position;
       obj.transform.rotation = Quaternion.identity;
       obj.SetActive(true);
 
-      yield return new WaitForSeconds(Waves[_currentWaveIndex].SpawnDelay);
+      yield return new WaitForSeconds(Waves[_waveIndex].SpawnDelay);
     }
   }
 }
